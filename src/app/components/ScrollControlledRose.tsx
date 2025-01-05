56daeb08493e54367db31b263b46f33da86e034d
yakatyansh <ykpetrolhead08@outlook.com>
2025-01-06 00:44:22 +0530

    more additions

--- src/app/components/ScrollControlledRose.tsx
+++ src/app/components/ScrollControlledRose.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client"
 
 import { motion, MotionValue, useTransform } from "framer-motion"
